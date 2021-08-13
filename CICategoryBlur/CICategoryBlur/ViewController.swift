3d3837b3f333804a9b9f5738e8c50d942dee6114
zhengwei <zhengwei_hy@migu.cn>
2021-08-13 09:37:59 +0800

    [add] 增加模糊

--- CICategoryBlur/CICategoryBlur/ViewController.swift
+++ CICategoryBlur/CICategoryBlur/ViewController.swift
@@ -13,63 +13,113 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        GaussianBlur()
+        ZoomBlur()
     }
     
     /// 方形模糊
     func BoxBlur() {
         let image = UIImage(named: "1.jpg")
         let ciImage = CIImage(image: image!)
-        let filter = CIFilter.init(name: "CIBoxBlur")
+        let filter = CIFilter(name: "CIBoxBlur")
         filter?.setValue(ciImage, forKey: "inputImage")
         filter?.setValue(10, forKey: "inputRadius")
         
         if let resultCIImage = filter?.outputImage {
-            let result = UIImage(ciImage: resultCIImage)
-            imageView.image = result
+            imageView.image = UIImage(ciImage: resultCIImage)
         }
     }
     /// 圆形模糊
     func DiscBlur() {
         let image = UIImage(named: "1.jpg")
         let ciImage = CIImage(image: image!)
-        let filter = CIFilter.init(name: "CIDiscBlur")
+        let filter = CIFilter(name: "CIDiscBlur")
         filter?.setValue(ciImage, forKey: "inputImage")
         filter?.setValue(10, forKey: "inputRadius")
         
         if let resultCIImage = filter?.outputImage {
-            let result = UIImage(ciImage: resultCIImage)
-            imageView.image = result
+            imageView.image = UIImage(ciImage: resultCIImage)
         }
     }
     /// 高斯模糊
     func GaussianBlur() {
         let image = UIImage(named: "1.jpg")
         let ciImage = CIImage(image: image!)
-        let filter = CIFilter.init(name: "CIGaussianBlur")
+        let filter = CIFilter(name: "CIGaussianBlur")
         filter?.setValue(ciImage, forKey: "inputImage")
         filter?.setValue(10, forKey: "inputRadius")
         
         if let resultCIImage = filter?.outputImage {
-            let result = UIImage(ciImage: resultCIImage)
-            imageView.image = result
+            imageView.image = UIImage(ciImage: resultCIImage)
         }
     }
     /// 遮罩模糊
     func MaskedVariableBlur() {
         
+        let image = UIImage(named: "1.jpg")
+        let ciImage = CIImage(image: image!)
+        
+        let maskImage = UIImage(named: "2.png")
+        let maskCIImage = CIImage(image: maskImage!)
+        
+        let filter = CIFilter(name: "CIMaskedVariableBlur")
+        filter?.setValue(ciImage, forKey: "inputImage")
+        filter?.setValue(10, forKey: "inputRadius")
+        filter?.setValue(maskCIImage, forKey: "inputMask")
+        
+        if let resultCIImage = filter?.outputImage {
+            imageView.image = UIImage(ciImage: resultCIImage)
+        }
     }
+    /// 均值模糊
     func MedianFilter() {
-        
+        let image = UIImage(named: "1.jpg")
+        let ciImage = CIImage(image: image!)
+
+        let filter = CIFilter(name: "CIMedianFilter")
+        filter?.setValue(ciImage, forKey: "inputImage")
+        if let resultCIImage = filter?.outputImage {
+            imageView.image = UIImage(ciImage: resultCIImage)
+        }
     }
+    /// 运动模糊
     func MotionBlur() {
-        
+        let image = UIImage(named: "1.jpg")
+        let ciImage = CIImage(image: image!)
+
+        let filter = CIFilter(name: "CIMotionBlur")
+        filter?.setValue(ciImage, forKey: "inputImage")
+        filter?.setValue(20, forKey: "inputRadius")
+        filter?.setValue(0, forKey: "inputAngle")
+        if let resultCIImage = filter?.outputImage {
+            imageView.image = UIImage(ciImage: resultCIImage)
+        }
     }
+    /// 降噪模糊
     func NoiseReduction() {
+        let image = UIImage(named: "1.jpg")
+        let ciImage = CIImage(image: image!)
+
+        let filter = CIFilter(name: "CINoiseReduction")
+        filter?.setValue(ciImage, forKey: "inputImage")
+        filter?.setValue(10, forKey: "inputNoiseLevel")
+        filter?.setValue(10, forKey: "inputSharpness")
         
+        if let resultCIImage = filter?.outputImage {
+            imageView.image = UIImage(ciImage: resultCIImage)
+        }
     }
+    /// 缩放模糊
     func ZoomBlur() {
-        
+        let image = UIImage(named: "1.jpg")
+        let ciImage = CIImage(image: image!)
+
+        let filter = CIFilter(name: "CIZoomBlur")
+        filter?.setValue(ciImage, forKey: "inputImage")
+        filter?.setValue(CIVector(x: imageView.frame.midX, y: imageView.frame.midY), forKey: "inputCenter")
+        filter?.setValue(20, forKey: "inputAmount")
+        if let resultCIImage = filter?.outputImage {
+            imageView.image = UIImage(ciImage: resultCIImage)
+        }
     }
 }
 
